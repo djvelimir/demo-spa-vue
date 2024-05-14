638ec40d07df53e18f8a17e6b8b4a550a4a8fb06
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-05-14 02:28:59 +0200

    Updates

--- js/about.f16c06eb.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkdemo_spa_vue"]=self["webpackChunkdemo_spa_vue"]||[]).push([[443],{4335:function(e,n,u){u.r(n),u.d(n,{default:function(){return f}});var a=u(3396);const s={class:"about"},t=(0,a._)("h1",null,"This is an about page",-1),r=[t];function c(e,n){return(0,a.wg)(),(0,a.iD)("div",s,r)}var o=u(89);const i={},d=(0,o.Z)(i,[["render",c]]);var f=d}}]);
-//# sourceMappingURL=about.f16c06eb.js.map
