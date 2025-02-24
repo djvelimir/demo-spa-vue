a1616e108088977644acb5f13c7bec5d39d9b2ed
Velimir Đurković <velimir.djurkovic@gmail.com>
2025-02-24 03:00:55 +0100

    feat: upgrade tailwind css from v3 to v4

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -10,23 +10,23 @@
           <div class="p-2 w-1/2">
             <div class="relative">
               <label for="name" class="leading-7 text-sm text-gray-400">Name</label>
-              <input type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              <input type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded-sm border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-hidden text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
           </div>
           <div class="p-2 w-1/2">
             <div class="relative">
               <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
-              <input type="email" id="email" name="email" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              <input type="email" id="email" name="email" class="w-full bg-gray-800 bg-opacity-40 rounded-sm border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-hidden text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
           </div>
           <div class="p-2 w-full">
             <div class="relative">
               <label for="message" class="leading-7 text-sm text-gray-400">Message</label>
-              <textarea id="message" name="message" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+              <textarea id="message" name="message" class="w-full bg-gray-800 bg-opacity-40 rounded-sm border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 h-32 text-base outline-hidden text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
           </div>
           <div class="p-2 w-full">
-            <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
+            <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-hidden hover:bg-green-600 rounded-sm text-lg">Button</button>
           </div>
           <div class="p-2 w-full pt-8 mt-8 border-t border-gray-800 text-center">
             <a class="text-green-400">example&#64;email.com</a>
--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
     <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
-    <div class="bg-green-500 text-white px-2 text-sm rounded rotate-12 absolute">
+    <div class="bg-green-500 text-white px-2 text-sm rounded-sm rotate-12 absolute">
       Page Not Found
     </div>
-      <router-link to="/home" class="mt-10 flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Go Home</router-link>
+      <router-link to="/home" class="mt-10 flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-hidden hover:bg-green-600 rounded-sm text-lg">Go Home</router-link>
   </main>
 </template>
